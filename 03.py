54a291d26882f548fd5090ab76519946885f9d3c
nebulasystems <asdie32@gmail.com>
2020-11-12 10:18:34 +0900

    연습문제 1단계

--- 03.py
+++ 03.py
@@ -1,5 +1,8 @@
 #3. for~in
 
-cnt = [1, 2, 3, 4, 5, 6, 7, 8, 9. 10]
+cnt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for w in cnt
+for w in cnt:
+    for r in range(w):
+        print('*', end='')
+    print('')
--- 04.py
+++ 04.py
@@ -1,1 +1,9 @@
 #4. 구구단 이중 for~in
+cnt1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+cnt2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for w in cnt1:
+    for r in cnt2:
+        rst = w * r
+        print(f'{r} × {w} = {rst} ', end='')
+    print('')
